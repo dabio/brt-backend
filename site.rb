28fbf795670e72382927f00d8ff90338731af594
dab <dbraband@gmail.com>
2011-02-20 22:05:46 +0100

    rewritten handlers and allow creation of a new person

--- site.rb
+++ site.rb
@@ -50,7 +50,11 @@ Cuba.define do
 
       @email = Email.new(contact)
       if @email.save
-        send_email(ENV['CONTACT_EMAIL'], :from => @email.email, :from_alias => @email.name, :subject => 'Nachricht vom berinracingteam.de', :body => @email.message)
+        send_email(ENV['CONTACT_EMAIL'],
+                   :from => @email.email,
+                   :from_alias => @email.name,
+                   :subject => 'Nachricht von berlinracingteam.de',
+                   :body => @email.message)
         @email.update(:send_at => Time.now)
         res.redirect '/kontakt'
       else
@@ -60,103 +64,119 @@ Cuba.define do
   end
 
 
-  # /news
-  on path('news') do
-    # /new
-    on path('new') do
-      # GET
-      on get do
-        break unless has_admin?
-        @news = News.new()
-        res.write render 'views/news_form.slim'
-      end
-
-      # POST
-      on post, param('news') do |news|
-        break unless has_admin?
-        @news = News.new(news)
-        @news.person = current_person
-        if @news.save
-          res.redirect @news.permalink
-        else
-          res.write render 'views/news_form.slim'
-        end
-      end
+  # /news/new
+  on path('news'), path('new') do
+    # GET
+    on get do
+      break unless has_admin?
+      @news = News.new()
+      res.write render 'views/news_form.slim'
     end
 
-    break
-
+    # POST
+    on post, param('news') do |news|
+      break unless has_admin?
+      @news = News.new(news)
+      @news.person = current_person
+      if @news.save
+        res.redirect @news.permalink
+      else
+        res.write render 'views/news_form.slim'
+      end
+    end
   end
 
+  # /news
+  on path('news') do
+    break
+  end
+
+
+  # /rennen/new
+  on path('rennen'), path('new') do
+    break unless has_admin?
+
+    on get do
+      @event = Event.new()
+      res.write render 'views/event_form.slim'
+    end
+
+    # POST
+    on post, param('event') do |event|
+      @event = Event.new(event)
+      @event.person = current_person
+      if @event.save
+        res.redirect @event.permalink
+      else
+        res.write render 'views/event_form.slim'
+      end
+    end
+  end
 
   # /rennen
   on path('rennen') do
-    # /new
-    on path('new') do
-      # GET
-      on get do
-        break unless has_admin?
-        @event = Event.new()
-        res.write render 'views/event_form.slim'
-      end
-
-      # POST
-      on post, param('event') do |event|
-        break unless has_admin?
-
-        @event = Event.new(event)
-        @event.person = current_person
-        if @event.save
-          res.redirect @event.permalink
-        else
-          res.write render 'views/event_form.slim'
-        end
-      end
-
-      break
-
-    end
-
     break
-
   end
 
 
-  # /team
-  on path 'team' do
-    # /first-lastname
-    on segment do |slug|
-      break not_found unless @person = Person.first(:slug => slug)
-      # /edit
-      on path 'edit' do
-        break not_found unless @person == current_person or has_admin?
-        # GET
-        on get do
-          res.write render 'views/person_form.slim'
-        end
-
-        # POST
-        on post, param('person') do |p|
-          @person.attributes = {
-            :email  => p['email'],
-            :info   => p['info']
-          }
-
-          @person.password = p['password'] unless p['password'].empty?
-          @person.password_confirmation = p['password_confirmation'] unless p['password'].empty?
-
-          if @person.save
-            res.redirect @person.permalink
-          else
-            res.write render 'views/person_form.slim'
-          end
-        end
-
-      end
-
+  # /team/fist-last/edit
+  on path('team'), segment, path('edit') do |slug|
+    @person = Person.first(:slug => slug)
+    break unless @person == current_person or has_admin?
+    # GET
+    on get do
+      res.write render 'views/person_form.slim'
     end
 
+    # POST
+    on post, param('person') do |p|
+      @person.attributes = {
+        :email  => p['email'],
+        :info   => p['info']
+      }
 
+      @person.password = p['password'] unless p['password'].empty?
+      @person.password_confirmation = p['password_confirmation'] unless p['password'].empty?
+
+      if @person.save
+        res.redirect @person.permalink
+      else
+        res.write render 'views/person_form.slim'
+      end
+    end
+  end
+
+  # /team/new
+  on path('team'), path('new') do
+    break unless has_admin?
+
+    # GET
+    on get do
+      @person = Person.new
+      res.write render 'views/person_form.slim'
+    end
+
+    # POST
+    on post, param('person') do |p|
+      @person = Person.new(p)
+
+      if @person.save
+        res.redirect @person.permalink
+      else
+        res.write render 'views/person_form.slim'
+      end
+    end
+  end
+
+  # /team/first-last
+  on path('team'), segment do |slug|
+    break unless @person = Person.first(:slug => slug)
+    break
+  end
+
+  # /team
+  on path('team') do
+    break
   end
 
 
