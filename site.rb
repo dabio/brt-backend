ab33673202516aaab252a2ef29bda64eaa24ae3b
dab <dbraband@gmail.com>
2011-03-25 23:12:14 +0100

    add new debates and comments to debates

--- site.rb
+++ site.rb
@@ -226,6 +226,37 @@ class BerlinRacingTeam < Sinatra::Base
   end
 
 
+  get '/diskussionen/new' do
+    not_found unless has_auth?
+
+    @debate = Debate.new
+    @comment = Comment.new
+    slim :debate_form
+  end
+
+
+  post '/diskussionen/new' do
+    not_found unless has_auth?
+
+    @debate = Debate.new params[:debate]
+    @debate.person = current_person
+    @comment = Comment.new params[:comment]
+    @comment.person = current_person
+
+    if @debate.save
+      @comment.debate = @debate
+      if @comment.save
+        redirect to(@debate.permalink)
+      else
+        @debate.destroy
+        slim :debate_form
+      end
+    else
+      slim :debate_form
+    end
+  end
+
+
   get '/diskussionen/:id' do
     not_found unless @debate = Debate.first(id: params[:id]) and has_auth?
     slim :debate
