e379f12ee3f54244e032a7961c50a0ff3be5537e
dab <dbraband@gmail.com>
2011-03-28 10:30:37 +0200

    seperate files for helper methods and config file

---
+++ config.rb
@@ -0,0 +1,31 @@
+# encoding: utf-8
+#
+#   this is berlinracingteam.de, a cuba application
+#   it is copyright (c) 2009-2011 danilo braband (danilo @ berlinracingteam,
+#   then a dot and a 'de')
+#
+
+class BerlinRacingTeam
+
+  register Sinatra::Flash
+  register Sinatra::R18n
+
+  use Rack::Session::Cookie
+
+  set :root, File.dirname(__FILE__)
+  set :cdn, '//berlinracingteam.commondatastorage.googleapis.com'
+
+  helpers Sinatra::BerlinRacingTeamHelper
+
+  helpers do
+    [:development, :production, :test].each do |environment|
+      define_method "#{environment.to_s}?" do
+        return settings.environment == environment.to_sym
+      end
+    end
+  end
+
+  configure :development do
+  end
+
+end
--- site.rb
+++ site.rb
@@ -8,126 +8,16 @@
 require 'bundler'
 Bundler.require
 
+# Sinatra::Base. This way, we're not polluting the global namespace with your
+# methods and routes and such.
+class BerlinRacingTeam < Sinatra::Base; end
+
 require_relative 'models/init'
+require_relative 'helpers'
+require_relative 'config'
 
-
-module Sinatra
-  module MainHelper
-
-    def today
-      @today = Date.today unless @today
-      @today
-    end
-
-  end
-
-  module PersonHelper
-
-    # This gives us the currently logged in user. We keep track of that by just
-    # setting a session variable with their is. If it doesn't exist, we want to
-    # return nil.
-    def current_person
-      @cp = Person.first(:id => session[:person_id]) if session[:person_id] unless @cp
-      @cp
-    end
-
-    # Checks if this is a logged in person
-    def has_auth?
-      !current_person.nil?
-    end
-
-    # Check if current person is logged in and admin
-    def has_admin?
-      has_auth? && current_person.id == 1
-    end
-
-  end
-
-  helpers PersonHelper
-
-
-  module TemplateHelper
-
-    # Taken from rails
-    AUTO_LINK_RE = %r{(?:([\w+.:-]+:)//|www\.)[^\s<]+}x
-    BRACKETS = {']' => '[', ')' => '(', '}' => '{'}
-    def auto_link(text, limit=nil)
-      trim = lambda {|s, l| l != nil and (s.length > limit and "#{s[0,l-1]}…") or s}
-      text.gsub(AUTO_LINK_RE) do
-        scheme, href = $1, $&
-        punctuation = []
-        # don't include trailing punctiation character as part of the URL
-        while href.sub!(/[^\w\/-]$/, '')
-          punctuation.push $&
-          if opening = BRACKETS[punctuation.last] and href.scan(opening).size > href.scan(punctuation.last).size
-            href << punctuation.pop
-            break
-          end
-        end
-
-        link_text = block_given? ? yield(href) : href
-        href = 'http://' + href unless scheme
-
-        "<a href=\"#{href}\">#{trim[link_text, limit]}</a>" + punctuation.reverse.join('')
-      end
-    end
-
-
-    def coat(file)
-      require 'digest/md5'
-      hash = Digest::MD5.file("views#{file}").hexdigest[0..4]
-      "#{file.gsub(/\.scss$/, '.css')}?#{hash}"
-    end
-
-    def footer
-      @events = Event.all(:date.gte => today, :order => [:date, :updated_at.desc],
-                          :limit => 3)
-      @people ||= Person.all(:order => [:last_name, :first_name])
-      slim :_footer
-    end
-
-    def simple_format str
-      str = '' if str.nil?
-      start_tag = '<p>'
-      end_tag = '</p>'
-      str.gsub! /\r\n?/, "\n"
-      str.gsub! /\n\n+/, "#{end_tag}\n\n#{start_tag}"
-      str.gsub! /([^\n]\n)(?=[^\n])/, '\1<br />'
-      str.insert 0, start_tag
-      str.concat end_tag
-    end
-
-  end
-
-  helpers TemplateHelper
-
-end
-
-
-class BerlinRacingTeam < Sinatra::Base
-  register Sinatra::Flash
-  register Sinatra::R18n
-
-  use Rack::Session::Cookie
-
-  set :root, File.dirname(__FILE__)
-  set :cdn, '//berlinracingteam.commondatastorage.googleapis.com'
-
-  helpers Sinatra::MainHelper
-  helpers Sinatra::PersonHelper
-  helpers Sinatra::TemplateHelper
-
-  helpers do
-    [:development, :production, :test].each do |environment|
-      define_method "#{environment.to_s}?" do
-        return settings.environment == environment.to_sym
-      end
-    end
-  end
-
-  configure :development do
-  end
-
+# It's a good form to make your Sinatra applications be a subclass of
+class BerlinRacingTeam
 
   get '/' do
     @news = News.all(:date.lte => today, :order => [:date.desc, :updated_at.desc],
