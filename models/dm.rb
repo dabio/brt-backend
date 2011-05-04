bda1b38b5a33e4096558bed75e21f6f117266464
dab <dbraband@gmail.com>
2011-05-04 07:39:34 +0200

    change description text from cuba to sinatra

--- models/dm.rb
+++ models/dm.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 #
-#   this is berlinracingteam.de, a cuba application
+#   this is berlinracingteam.de, a sinatra application
 #   it is copyright (c) 2009-2011 danilo braband (danilo @ berlinracingteam,
 #   then a dot and a 'de')
 #
@@ -8,9 +8,9 @@
 module DataMapper
   class Property
 
-    autoload :BCryptHash,         root_path('models/dm/bcrypt_hash')
-    autoload :Enum,               root_path('models/dm/enum')
-    autoload :URI,                root_path('models/dm/uri')
+    autoload :BCryptHash, root_path('models/dm/bcrypt_hash')
+    autoload :Enum,       root_path('models/dm/enum')
+    autoload :URI,        root_path('models/dm/uri')
 
   end
 end
--- models/dm/enum.rb
+++ models/dm/enum.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 #
-#   this is berlinracingteam.de, a cuba application
+#   this is berlinracingteam.de, a sinatra application
 #   it is copyright (c) 2009-2011 danilo braband (danilo @ berlinracingteam,
 #   then a dot and a 'de')
 #
