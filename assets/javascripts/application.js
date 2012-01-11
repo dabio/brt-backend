37f6f90ec9f3f8c8d3cbcd463cf5c98bdc0ed67d
dab <dbraband@gmail.com>
2012-01-11 17:56:19 +0100

    Use own js implementation instead of ender, because we only need that for one case. Case solved with simple js functions.

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,38 +1,17 @@
-//= require ender.min
+document.addEventListener('DOMContentLoaded', function(event) {
+    var $ = function(id) {
+            return document.getElementById(id) || !1;
+        },
+        email_el = $('email');
 
-// replace encoded emails
-$('a.email').each(function(el) {
-  if (!el.innerHTML) return !1;
-  var email = el.innerHTML.replace(' . ', '.').replace(' [at] ', '@');
-  el.href = 'mailto:'+email;
-  el.innerHTML = email;
-});
-/*
-// edit link
-$('a.edit').click(function(el) {
-  var a = new Array(
-        $('.news-item'),
-        $('.news-item-aside'),
-        $('.news-form'),
-        $('.news-form-aside')
-      ),
-      i;
-
-  for (i=0; i < a.length; i++) {
-    a[i].toggle(function(){}, 'block');
-  }
+    if (email_el) {
+        if (!email_el.innerHTML) return !1;
+        var email = email_el.innerHTML.replace(' . ', '.').replace(' [at] ', '@');
+        email_el.href = 'mailto:'+email;
+        email_el.innerHTML = email;
+    }
 
 });
 
-// delete link
-$('a.delete').click(function(el) {
-  alert('delete');
-});
 
-// button submit
-//$('.news-form button').click(function(el) {
-//  el.preventDefault();el.stopPropagation();
-//  //$('form')
-//  //$.ajax();
-//});
-*/
+
