ae4b5520fb1a8b0d58abd78a3848d12703500845
dab <dbraband@gmail.com>
2012-01-11 17:33:45 +0100

    Detail view for events without any event report

---
+++ views/event_detail.rb
@@ -0,0 +1,59 @@
+# encoding: utf-8
+#
+#   this is berlinracingteam.de, a sinatra application
+#   it is copyright (c) 2009-2012 danilo braband (danilo @ berlinracingteam,
+#   then a dot and a 'de')
+#
+
+class App
+  module Views
+    class EventDetail < Layout
+
+      def title
+        "#{@event.title} - Berlin Racing Team"
+      end
+
+      def nav_rennen
+        'active'
+      end
+
+      def link_to_year
+        "/rennen/#{@event.date.year}"
+      end
+
+      def event
+        {
+          title: @event.title,
+          date: l(@event.date, :full),
+          distance: @event.distance
+        }
+      end
+
+      def participations?
+        not @event.participations.empty?
+      end
+
+      def participations
+        @event.participations.map do |p|
+          {
+            person: person(p.person),
+            position_overall: p.position_overall,
+            position_age_class: p.position_age_class
+          }
+        end
+      end
+
+      def person(p)
+        return nil unless p
+        {
+          name: p.name,
+          permalink: p.permalink
+        }
+      end
+
+    end
+  end
+end
+
+
+
