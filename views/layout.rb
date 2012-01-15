614b7c10135cb602e88537f8bfec29caf7df7c66
dab <dbraband@gmail.com>
2012-01-15 23:02:12 +0100

    Show date in footer and correct paddings.

--- views/layout.rb
+++ views/layout.rb
@@ -19,11 +19,16 @@ class App
       end
 
       def has_next_races
-        next_races().length > 0
+        next_races.length > 0
       end
 
       def next_races
-        Event.all(:date.gte => Date.today, limit: 3)
+        Event.all(:date.gte => Date.today, limit: 3).map do |e|
+          {
+            title: e.title,
+            date: l(e.date, :full)
+          }
+        end
       end
 
       def today
