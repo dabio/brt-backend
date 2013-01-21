5a810391603f32b36bc0ba14a554af584983fff2
dabio <dbraband@gmail.com>
2013-01-21 21:35:06 +0100

    Current version.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,9 +1,15 @@
 class Person < ActiveRecord::Base
+  has_secure_password
+
   attr_accessible :email, :first_name, :info, :is_admin, :last_name, :password, :password_confirmation, :slug
 
   validates :first_name,  presence: true, length: { maximum: 50 }
   validates :last_name,   presence: true, length: { maximum: 50 }
-  #validates :email,       presence: true, email: true, uniqueness: true, length: { maximum: 255, minumum: 5 }
+  validates :email,       presence: true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create }, uniqueness: true, length: { maximum: 255, minumum: 5 }
   validates :password,    presence: true, confirmation: true,
     unless: Proc.new { |a| a.password.blank? }
+
+  def self.authenticate(email, password)
+    find_by_email(email).try(:authenticate, password)
+  end
 end
--- test/unit/person_test.rb
+++ test/unit/person_test.rb
@@ -1,7 +1,46 @@
 require 'test_helper'
 
 class PersonTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "for validation" do
+    # Person needs at least a first, last name and email address.
+    person = Person.new({first_name: 'FirstName'})
+    assert !person.valid?
+
+    person.last_name = 'LastName'
+    assert !person.valid?
+
+    person.email = 'firstname@lastname.com'
+    assert person.valid?
+  end
+
+  test "for valid email address" do
+    person = Person.new({first_name: 'FirstName', last_name: 'LastName'})
+    assert !person.valid?
+    # invalid address - missing name
+    person.email = 'blah.com'
+    assert !person.valid?
+    # invalid address - missing domain
+    person.email = 'test@.com'
+    assert !person.valid?
+    # invalid address - missing tld
+    person.email = 'test@test'
+    assert !person.valid?
+    # valid address
+    person.email = 'test@test.com'
+    assert person.valid?
+  end
+
+  test "for password confirmation" do
+    person = Person.new({first_name: 'FirstName', last_name: 'LastName', email: 'test@test.com'})
+    assert person.valid?
+    # wrong confirmation password
+    person.password = 'test1234'
+    person.password_confirmation = 'blah1234'
+    assert !person.valid?
+    # correct confirmation password
+    person.password_confirmation = 'test1234'
+    assert person.valid?
+  end
+
 end
